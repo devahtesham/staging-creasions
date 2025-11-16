5e265d7cf961035e77d2dc9ec1446b1b23151504
Ahtesham Akram <ahte02254@gmail.com>
2025-11-16 14:42:52 +0500

    remove old case study components and styles; update sitemap with new blog URLs

--- src/components/views/case-study/hurghada-page/Section01.jsx
+++ src/components/views/case-study/hurghada-page/Section01.jsx
@@ -6,21 +6,27 @@ import bannerMobile from '/public/hurghada-image/banner-Mobile.png'
 import bannerstartvector from '/public/hurghada-image/banner-start-vector.png'
 
 export default function Section01({ data }) {
+    const backgroundImage = data?.hero_image || bannerImage.src
+    const title = data?.title || 'Case Study'
+    const subtitle = data?.subtitle || '<h1>Welcome to Hurghada</h1>'
+    const logoImage = data?.logo || logo.src
+    const mobileImage = data?.background_image || bannerMobile.src
+
     return (
-        <section className="heroBanner" style={{ backgroundImage: `url(${data?.hero_image})` }}>
-            <Image src={bannerstartvector} alt="image" className='bannerstartvector' />
+        <section className="heroBanner" style={{ backgroundImage: `url(${backgroundImage})` }}>
+            <Image src={bannerstartvector} alt="image" className='bannerstartvector' width={550} height={550} />
             <div className="container">
                 <div className="row">
                     <div className="col-lg-6">
                         <div className="text">
-                            <h6> {data?.title}</h6>
-                            <h1 dangerouslySetInnerHTML={{ __html: data?.subtitle }} />
+                            <h6>{title}</h6>
+                            <h1 dangerouslySetInnerHTML={{ __html: subtitle }} />
                         </div>
                     </div>
                     <div className="col-lg-6">
                         <div className="imgBox">
-                            <Image src={data?.logo || logo.src} alt="image" className='bannerLogo' width={290} height={105} />
-                            <Image src={data?.background_image || bannerMobile.src} alt="image" width={471} height={962} />
+                            <Image src={logoImage} alt="image" className='bannerLogo' width={290} height={105} />
+                            <Image src={mobileImage} alt="image" width={471} height={962} />
                         </div>
                     </div>
                 </div>
